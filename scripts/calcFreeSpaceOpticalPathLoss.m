d8f8a01bf42003456651f3a7467c95a42eacdfa6
Adrian Salustri <45179354+trilusa@users.noreply.github.com>
2022-01-11 12:36:36 -0500

    moved monte carlo data, added data loading script,

---
+++ scripts/calcFreeSpaceOpticalPathLoss.m
@@ -0,0 +1,57 @@
+v = 0.0:.2:.8;   %horizonal positions
+D_d = .004:.002:.016;  %diode diameters to test
+Pt=1;  % Tx power, 1 watt
+h=1.5; %m
+m=1; %lambertian source with half angle = 60 degrees
+
+%initialize variables to hold results
+Pr = zeros(length(D_d), length(v)); % holds absolute power values
+Pr_db = zeros(length(D_d), length(v)); % holds power in dB wrt power recived at v=0
+
+%for every diode and position, calculate the receiever power
+for d=1:length(D_d)
+As = pi*(D_d(d)/2)^2; %calc sensing area
+for i = 1:length(v)
+    alpha = calcAlphaForPDWithArea(D_d(d),v(i)); %calc alpha
+    dist = sqrt(v(i)^2+h^2); %distance from light to corcnercube
+    theta = atan(v(i)/h); %incidence angle wrt CC normal
+
+    %This is eqn 7 in info com paper
+    Pr(d,i)= Pt * (((m+1)*As) / (8*pi*dist^2)) * (cos(theta)^(m+1)) * alpha;
+    Pr_db(d,i) = 20*log10(Pr(d,i)/Pr(d,1));
+
+   %power calc goes negative when ERA = 0, so log10() become imaginary
+   if imag(Pr_db(d,i)) ~= 0
+       Pr_db(d,i) = -inf;
+   end
+
+end
+end
+
+%%plotting
+clf
+t=tiledlayout('flow');
+nexttile
+plot(v,Pr(1:4:end,:),'-')
+lgn_txt = repmat("PD diam = ", length(D_d(1:4:end)), 1) + num2str(D_d(1:4:end)'*1000) + repmat(" mm",length(D_d(1:4:end)),1);
+lgd=legend(lgn_txt);
+title("Horizontal Dist vs Rx Power")
+% ylabel("P (dB_{FS})")
+ylabel("P (W)")
+
+xlabel("Horizontal Displacement (m)")
+set(lgd,'location','northeast')
+grid on
+
+
+nexttile
+plot(D_d*1000,Pr(:,1:10:end))
+lgn_txt = repmat("v = ", length(v(1:10:end)), 1) + num2str(v(1:10:end)'*1000) + repmat(" mm",length(v(1:10:end)),1);
+lgd=legend(lgn_txt);
+title("PD diam vs Rx Power")
+% ylabel("P (dB_{FS})")
+ylabel("P (W)")
+
+set(lgd,'location','northwest')
+xlabel("PD Diameter (mm)")
+grid on
---
+++ scripts/loadData.m
@@ -0,0 +1,25 @@
+
+D_d_sim = 0.004;%:0.002:0.016;     % detector diameter (1mm to simulate point source)
+v_sim = 0:0.2:.8;    % horizontal displacements to simulate
+
+suffix = 1:25;
+N=2e6;
+data = zeros(length(D_d_sim),length(v));
+for s = suffix
+data_all_temp=[];
+for d=1:length(D_d_sim)
+    data_temp = [];
+    fn =['monte-carlo-alpha_N' num2str(N) '_PD' num2str(D_d_sim(d)*1000) 'mm_L' num2str(50) 'mm(' num2str(suffix(s)) ').mat'];
+    load(fn, 'detected_this_trial');
+    for i=1:length(detected_this_trial)
+        data_temp = [data_temp length(detected_this_trial{i})];
+    end
+    data_all_temp = [data_all_temp; data_temp];
+end
+data = data + data_all_temp;
+end
+thetas = atand(v_sim/h);
+data_cos  = data .* cosd(thetas);
+data_cos_d = data_cos ./ (2* sqrt(v_sim.^2 + h^2));
+data_cos_d_norm = data_cos_d ./ data_cos_d(:,1);
+
